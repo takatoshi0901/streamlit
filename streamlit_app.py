88bcd1633cd10676102e99e2267acba9caaa2159
naochika0110 <cowntdowntoskyscraper@yahoo.co.jp>
2025-06-03 02:21:41 +0900

    fix: ラベルを修正

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,19 +8,19 @@ st.title("Streamlit 単体アプリ")
 
 # CSVファイルの処理
 st.header("CSVファイル処理（先頭5行の抽出）")
-uploaded_csv = st.file_uploader("CSVファイルをアップロードしてください", type=["csv"])
+uploaded_csv = st.file_uploader("CSVファイルをアップロードしてね", type=["csv"])
 
 if uploaded_csv is not None:
     df = pd.read_csv(uploaded_csv)
-    st.write("ファイルを処理しました。先頭5行は以下の通りです：")
+    st.write("ファイルを処理。先頭5行はこちら：")
     st.write(df.head())
 
 # 画像ファイルの処理
-st.header("画像ファイル処理（グレースケール）")
-uploaded_img = st.file_uploader("画像ファイルをアップロードしてください", type=["png", "jpg", "jpeg"])
+st.header("画像ファイル処理→グレー")
+uploaded_img = st.file_uploader("画像ファイルをアップロードしてね", type=["png", "jpg", "jpeg"])
 
 if uploaded_img is not None:
     image = Image.open(uploaded_img)
     gray_image = image.convert("L")
-    st.write("画像をグレースケールに変換しました。")
-    st.image(image)
+    st.write("画像をグレーに変換したよ。")
+    st.image(gray_image)
